8002ce32ebc291679c52dd1c78dddf462aada7d7
Helgurd <rekauta@gmail.com>
2021-08-06 11:03:54 +0000

    some codes has been changed while used as a basice  game fundemntals to functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -48,7 +48,7 @@ console.log(btn );
 // creating an Array of words to scramble
 const myWords = ["hi","bird","dog","cat","cow"]; //min 2 character words or we end up in a 
 //information about the words inside game g
-const game = {sel:'',scramble:'',score:0,incorrect:0, wordsLeft: 0, played:myWords.length};
+const game = {sel:'',scramble:'',score:0,incorrect:0,wordsLeft:0,played:myWords.length};
 
 //event listner to listn when pressed the button. and we will add event tracker object using arrow format for the function.
 
@@ -56,65 +56,58 @@ const game = {sel:'',scramble:'',score:0,incorrect:0, wordsLeft: 0, played:myWor
 btn.addEventListener('click',(e)=>{
     //console.log(myWords);
     //score value has to be set to reseted eachtimes the game will start
-    scoreBoard.textContent = 'Score: 0';
-    scoreBoard.style.display = 'block';
-    inWord.style.display = 'inline';
-      //remove the button while clikcking it
-    btn.style.display = 'none';
-    //console.log(myWords);
-    /*the sort is expecting a function to be returned back and by adding (-) in front of random it will subtracting whatever the value is 
-     and will return defferent order,  by this will allow us to scrample the words.*/
-    myWords.sort(()=>{ return 0.5 - Math.random()});
-        //out puting the first word with the index value of [0] to the outputing area. 
-        //the word has to be removed and update with next random one with the shift(); methode.
-    //game.sel = myWords[0];
-    game.sel = myWords.shift(); 
-    //chek how many words left after first attemtions.
-    game.wordsLeft = myWords.length;
+    // we will replace all previouse values in If else  statemnt to evalute the length of the words 
 
-        //we will pass here whatever returend fro mthe functions sorter and the value will be the selective word.
+    if(myWords.length <= 0){
+        console.log('game over');
+        gameArea.innerHTML = `<div>GAME OVER</div>`;
+        gameArea.innerHTML += `<div>You Got ${game.score} correct vs ${game.incorrect} incorrect out of ${game.played} words total</div>`
+    }else{
 
-    game.scramble = sorter(game.sel);
-    //font size
-    output.style.fontSize = '3em';
-    output.style.letterSpacing = '0.5em';
-    //to have the selected word or the scrambled word, the same length we have to set the attraibute max and length, in another word we will have the same length between input and scrambled words and more words will not alowed.
+        inWord.disabled = false;
+        inWord.value = "";
+        inWord.style.borderWidth = '1px';
+        inWord.style.borderColor = '#eee';
+        scoreBoard.style.display = 'block';
+        inWord.style.display = 'inline';
+        btn.style.display = 'none';
+        myWords.sort(()=>{ return 0.5 - Math.random()});
+        game.sel = myWords.shift();
+        game.wordsLeft = myWords.length;
+        console.log(game);
+        game.scramble = sorter(game.sel);
+    
+        addScore();   
+        output.style.fontSize = '3em';
+        output.style.letterSpacing = '0.5em';
+        inWord.setAttribute('maxlength',game.sel.length);
+        inWord.focus();
+        output.textContent = `${game.scramble}`;
+        console.log(game.sel,game.scramble);
+        }
+    })
 
-    inWord.setAttribute('maxlength',game.sel.length);
-
-    //we will put fucs to the input area wehnever we open the browser.
-
-    inWord.focus();
-    //while the word has been selected , we can scramble that word and turn it to an array.
-    output.textContent = `${game.scramble}`;
-        //we make sure we not getting back the word selected as the value of word scrambled. 
-    console.log(game.sel,game.scramble);
-})
-
-/* letter count event listoner that will wapply to the inWord element
- the cose has testd whether is working or not and the image in the test folder*/
-inWord.addEventListener('keyup',(e)=>{
-        /*after the key has been tracked and tested and added to the event we going to repalce this code with (e) eventlistinor reference. 
-    console.log(inWord.value.length)*/
+// Adding an method to liston to the event keyup
+inWord.addEventListener('keyup', (e)=>{
     console.log(e);
+    
     inWord.style.borderColor = '#eee';
     inWord.style.borderWidth = '1px';
-     /*if  value of the length is equal to game selectd length word or event code equal to Enter
-        we will  run the cheking */
     if(inWord.value.length == game.sel.length || e.code == 'Enter'){
         winChecker();
     }
-})
+    
+    
+    })
+
+    function addScore(){
+        let tempOutput = `Score : <b>${game.score}</b> vs incorrect <i>(${game.incorrect})</i><small> ${game.wordsLeft} words left </small>`;
+        scoreBoard.innerHTML = tempOutput;
+    }
 
 
 
-function addScore(){
-    let tempOutput = `Score : <b>${game.score}</b> vs incorrect <i>(${game.incorrect})</i><small> ${game.wordsLeft} words left </small>`;
-    scoreBoard.innerHTML = tempOutput;
-}
-
-
-// check if  the words are matching. we only passing the global value/ 
+    // check if  the words are matching. we only passing the global value/ 
 function winChecker(){
     inWord.style.borderWidth = '5px';
     if(inWord.value == game.sel){
@@ -135,6 +128,11 @@ function winChecker(){
     addScore();
 }
 
+
+
+
+
+
 /*
 to fix the bug that words are not scrambled sometimes 
 ex: world  will show the same world after scrambling. 
@@ -154,3 +152,64 @@ function sorter(val){
     return temp;
 }
  
+
+
+//     scoreBoard.textContent = 'Score: 0';
+//     scoreBoard.style.display = 'block';
+//     inWord.style.display = 'inline';
+//       //remove the button while clikcking it
+//     btn.style.display = 'none';
+//     //console.log(myWords);
+//     /*the sort is expecting a function to be returned back and by adding (-) in front of random it will subtracting whatever the value is 
+//      and will return defferent order,  by this will allow us to scrample the words.*/
+//     myWords.sort(()=>{ return 0.5 - Math.random()});
+//         //out puting the first word with the index value of [0] to the outputing area. 
+//         //the word has to be removed and update with next random one with the shift(); methode.
+//     //game.sel = myWords[0];
+//     game.sel = myWords.shift(); 
+//     //chek how many words left after first attemtions.
+//     game.wordsLeft = myWords.length;
+
+//         //we will pass here whatever returend fro mthe functions sorter and the value will be the selective word.
+
+//     game.scramble = sorter(game.sel);
+//     //font size
+//     output.style.fontSize = '3em';
+//     output.style.letterSpacing = '0.5em';
+//     //to have the selected word or the scrambled word, the same length we have to set the attraibute max and length, in another word we will have the same length between input and scrambled words and more words will not alowed.
+
+//     inWord.setAttribute('maxlength',game.sel.length);
+
+//     //we will put fucs to the input area wehnever we open the browser.
+
+//     inWord.focus();
+//     //while the word has been selected , we can scramble that word and turn it to an array.
+//     output.textContent = `${game.scramble}`;
+//         //we make sure we not getting back the word selected as the value of word scrambled. 
+//     console.log(game.sel,game.scramble);
+// })
+
+
+
+
+
+// /* letter count event listoner that will wapply to the inWord element
+//  the cose has testd whether is working or not and the image in the test folder*/
+// inWord.addEventListener('keyup',(e)=>{
+//         /*after the key has been tracked and tested and added to the event we going to repalce this code with (e) eventlistinor reference. 
+//     console.log(inWord.value.length)*/
+//     console.log(e);
+//     inWord.style.borderColor = '#eee';
+//     inWord.style.borderWidth = '1px';
+//      /*if  value of the length is equal to game selectd length word or event code equal to Enter
+//         we will  run the cheking */
+//     if(inWord.value.length == game.sel.length || e.code == 'Enter'){
+//         winChecker();
+//     }
+// })
+
+
+
+
+
+
